56fbd6fffaf41b03fd40e4e459005ca6a8e37457
SerhioKhavanski <serhiobladestone@gmail.com>
2026-05-06 20:11:16 +0300

    Added project menu

---
+++ src/components/CategoryButton.jsx
@@ -0,0 +1,9 @@
+function CategoruButton ({category,activeCategory,setActiveCategory}){
+
+    return(
+        <button onClick={()=>setActiveCategory(category)}  type='button' className={activeCategory === category?("px-5 py-2 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 text-white text-sm font-medium shadow-lg shadow-purple-500/20 transition hover:scale-105"):("px-5 py-2 rounded-full glass text-sm font-medium hover:border-purple-400/30 hover:scale-105 transition")}>
+            {category}
+        </button>
+    )
+}
+export default CategoruButton
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,56 @@
+import { useState } from 'react'
+import CategoruButton from './CategoryButton.jsx'
 const projects = [
-    {title:"Landing Page", description:"My first project"},
-    {img:"/valdevslave-portfolio/img/Screenshot 2026-05-06 094954.png",title:"Portfolio", description:"Personal website"},
-    {title:"JS App", description:"Small JavaScript app"}
+    {title:"Landing Page", description:"My first project", category: "React"},
+    {img:"/valdevslave-portfolio/img/Screenshot 2026-05-06 094954.png",title:"Portfolio", description:"Personal website", category: "C#"},
+    {title:"JS App", description:"Small JavaScript app", category: "Fullstack"}
 ]
+const projectMenu = ["All", "React", "C#", "UI/UX", "Fullstack"]
 
 import ProjectCard from './ProjectCard.jsx'
 
 function Projects(){
+
+    const [activeCategory,setActiveCategory] = useState("All")
+    const filteredProjects = projects.filter(project =>{
+        if(activeCategory === "All"){
+            return true
+        }
+        if(project.category === activeCategory){
+            return true
+        }
+    })
+
     return (
         <section className="px-6 py-24">
         <div className="max-w-7xl mx-auto space-y-12">
 
             <h2 className="text-3xl md:text-4xl font-bold text-center">
-            Projects
+                Projects
             </h2>
 
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {projects.map((project, index) => (
-                <ProjectCard project={project} key={index} />
-            ))}
+            <div className="flex flex-wrap justify-center gap-3">
+                {projectMenu.map((menuPoint) => (
+                    <CategoruButton 
+                    category ={menuPoint} 
+                    activeCategory = {activeCategory}
+                    setActiveCategory = {setActiveCategory}/>
+                ))}
             </div>
 
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {filteredProjects.length>0?(
+                filteredProjects.map((project, index) => (
+                    <ProjectCard project={project} key={index} />
+                )) 
+            ):(
+                <div className="col-span-full flex justify-center items-center py-20">
+                    <p className="glass px-8 py-4 rounded-2xl text-sm md:text-base text-black/60 dark:text-white/70 tracking-wide">
+                        Таких проектов пока нет
+                    </p>
+                </div>
+            )}  
+            </div>
         </div>
         </section>
     )
